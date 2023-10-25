7be4cf376c66aa7ebcaeb100c64ebed8a062ded0
CloudRazerz <146293992+CloudRazerz@users.noreply.github.com>
2023-10-25 18:20:53 -0400

    main function

--- Lab6.py
+++ Lab6.py
@@ -10,3 +10,26 @@ def encode(password):
     for digit in password:
         result += str(int(digit) + 3)
     return str(result)
+
+def decode(password):
+    return
+
+if __name__ == "__main__":
+    running = True
+    password = int
+    while running:
+        print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+              """)
+        choice = int(input("Please enter an option: "))
+        if choice == 1:
+            password = encode(input("Please enter your password to encode: "))
+            print("Your password has been encoded and stored!")
+        if choice == 2:
+            print(f"The encoded password is {password}, and the original password is {decode(password)}.")
+        if choice == 3:
+            running = False
+            break
